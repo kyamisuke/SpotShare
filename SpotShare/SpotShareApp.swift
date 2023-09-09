64b1a404419cb654d5321bbe9be95b31b8215769
kyamisuke <murakami.k.am@m.titech.ac.jp>
2023-09-09 18:51:02 +0900

    Initial Commit

---
+++ SpotShare/SpotShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotShareApp.swift
+//  SpotShare
+//
+//  Created by 村上航輔 on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct SpotShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
